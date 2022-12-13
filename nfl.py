3f4a9b2db5056ff5fedbf7bf5b130c451fb40bae
dafg05 <danialefloresg@gmail.com>
2022-12-12 20:45:43 -0500

    parity map and compute mapppings

--- nfl.py
+++ nfl.py
@@ -3,7 +3,7 @@ from typing import Callable
 
 DOMAIN = range(10)
 
-def distFromMap(map: Callable):
+def dist_from_Map(map: Callable):
     """
     Define a distribution where the probabilities are uniform over all
     values that agree with mapping, zero otherwise
@@ -19,3 +19,18 @@ def distFromMap(map: Callable):
         dist[(x, b)] = p
         dist[(x, not b)] = 0
     return dist
+
+def parity_map(x):
+    if x % 2 == 0:
+        return True
+    else:
+        return False
+
+def compute_mappings(map):
+    mappings = {}
+    for x in DOMAIN:
+        mappings[x] = map(x)
+    return mappings
+
+if __name__ == "__main__":
+    raise NotImplementedError
