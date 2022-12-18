4abc9c64c80d6c3baca917345c365d3d358f7571
dafg05 <danialefloresg@gmail.com>
2022-12-18 14:19:09 -0600

    starting to migrate from maps.py to algos.py

--- maps.py
+++ maps.py
@@ -1,13 +1,23 @@
 from constants import DOMAIN
+import random as rand
+
+# TODO: redefine to algorithms.py
 
 def parity_map(x):
+# DEPRECATED: not actually an algorithm
     if x % 2 == 0:
         return 1
     else:
         return 0
 
-def true_map(x):
-    return 1
+def die_by_one(S):
+    def f(x):
+        return 1
+    return f
 
-def false_map(x):
-    return 0
+def die_by_zero(S):
+    def f(x):
+        return 0
+    return f
+
+        
